f6a39350378991387ee2b3a1a77cd9d5542ccb6d
IsaacABG <131383886+IsaacABG@users.noreply.github.com>
2023-05-11 12:06:18 -0600

    Update hello-openshift-for-developers.go
    cambio de texto hola mundo

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hola mundo! :)"
+		response = "Hola mundo!"
 	}
 
 	fmt.Fprintln(w, response)
